ec8d573d405f03df5fcbfe0024e42a761ba5a544
ai-kurou <ai-kurou@gmail.com>
2026-06-06 14:03:45 +0900

    Revert "feat: Windows TTSを常駐プロセス化し読み上げ中の新規テキストを破棄"
    This reverts commit 2a058f3789ee39344015d4d7d1b036da109158c7.

--- app/desktopApp/src/main/kotlin/kurou/kodriver/tts/WindowsTtsEngine.kt
+++ app/desktopApp/src/main/kotlin/kurou/kodriver/tts/WindowsTtsEngine.kt
@@ -3,34 +3,18 @@ package kurou.kodriver.tts
 import kurou.kodriver.domain.engine.TextToSpeechEngine
 
 internal class WindowsTtsEngine : TextToSpeechEngine {
-
-    private val process: Process = ProcessBuilder(
-        "powershell",
-        "-NoProfile",
-        "-NonInteractive",
-        "-Command",
-        """
-        Add-Type -AssemblyName System.Speech
-        ${'$'}tts = New-Object System.Speech.Synthesis.SpeechSynthesizer
-        while (${'$'}true) {
-            ${'$'}line = [Console]::ReadLine()
-            if (${'$'}line -eq ${'$'}null) { break }
-            if (${'$'}tts.State -eq [System.Speech.Synthesis.SynthesizerState]::Ready) {
-                ${'$'}tts.SpeakAsync(${'$'}line)
-            }
-        }
-        """.trimIndent(),
-    ).start()
-
-    private val writer = process.outputStream.bufferedWriter()
-
     override fun speak(text: String) {
-        writer.write(text.replace("\n", " "))
-        writer.newLine()
-        writer.flush()
+        val safeText = text.replace("'", "''")
+        ProcessBuilder(
+            "powershell",
+            "-NoProfile",
+            "-NonInteractive",
+            "-Command",
+            "Add-Type -AssemblyName System.Speech; " +
+                "\$tts = New-Object System.Speech.Synthesis.SpeechSynthesizer; " +
+                "\$tts.Speak('$safeText')",
+        ).start()
     }
 
-    override fun stop() {
-        process.destroy()
-    }
+    override fun stop() = Unit
 }
