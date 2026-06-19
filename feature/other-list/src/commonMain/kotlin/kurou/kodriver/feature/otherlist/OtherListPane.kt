8e7db2a48c5bfb00c5a38aed1cddecb11ef0f993
ai-kurou <ai-kurou@gmail.com>
2026-06-19 14:25:59 +0900

    バージョン表示行に区切り線を追加

--- feature/other-list/src/commonMain/kotlin/kurou/kodriver/feature/otherlist/OtherListPane.kt
+++ feature/other-list/src/commonMain/kotlin/kurou/kodriver/feature/otherlist/OtherListPane.kt
@@ -182,6 +182,7 @@ fun OtherListPane(
                 appVersionLabel = uiState.appVersionLabel,
                 appVersion = uiState.appVersion,
             )
+            HorizontalDivider()
         }
     }
 }
