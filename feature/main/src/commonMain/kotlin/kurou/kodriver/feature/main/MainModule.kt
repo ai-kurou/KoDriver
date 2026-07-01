fd3ae0e94dba0cd043126f7e0f01e509e8590074
ai-kurou <ai-kurou@gmail.com>
2026-07-02 08:43:53 +0900

    スリープ防止設定UseCaseのDIを追加

--- feature/main/src/commonMain/kotlin/kurou/kodriver/feature/main/MainModule.kt
+++ feature/main/src/commonMain/kotlin/kurou/kodriver/feature/main/MainModule.kt
@@ -2,7 +2,9 @@ package kurou.kodriver.feature.main
 
 import kurou.kodriver.domain.usecase.CheckAppUpdateAvailableUseCase
 import kurou.kodriver.domain.usecase.ObserveExitConfirmationEnabledUseCase
+import kurou.kodriver.domain.usecase.ObserveKeepScreenOnUseCase
 import kurou.kodriver.domain.usecase.SaveExitConfirmationEnabledUseCase
+import kurou.kodriver.domain.usecase.SaveKeepScreenOnUseCase
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.viewModel
 import org.koin.core.module.dsl.viewModelOf
@@ -11,7 +13,9 @@ import org.koin.dsl.module
 val mainModule = module {
     factory { CheckAppUpdateAvailableUseCase(get()) }
     factory { ObserveExitConfirmationEnabledUseCase(get()) }
+    factory { ObserveKeepScreenOnUseCase(get()) }
     factory { SaveExitConfirmationEnabledUseCase(get()) }
+    factory { SaveKeepScreenOnUseCase(get()) }
     viewModel { AppScreenViewModel(get(), currentAppVersion(), get(), get(), get()) }
     viewModelOf(::ConnectionBannerViewModel)
 }
