93cb98a03351f37bffbd65b273989d178219a47d
ai-kurou <ai-kurou@gmail.com>
2026-07-02 21:57:22 +0900

    Androidの戻る操作を予測型戻るに対応

--- app/androidApp/src/main/kotlin/kurou/kodriver/MainActivity.kt
+++ app/androidApp/src/main/kotlin/kurou/kodriver/MainActivity.kt
@@ -2,18 +2,30 @@ package kurou.kodriver
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.BackHandler
+import androidx.activity.ExperimentalActivityApi
+import androidx.activity.compose.PredictiveBackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import kotlinx.coroutines.CancellationException
 import kurou.kodriver.presentation.AppScreen
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalActivityApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             AppScreen(
-                backHandler = { enabled, onBack -> BackHandler(enabled = enabled, onBack = onBack) },
+                backHandler = { enabled, onBack ->
+                    PredictiveBackHandler(enabled = enabled) { progress ->
+                        try {
+                            progress.collect {}
+                            onBack()
+                        } catch (e: CancellationException) {
+                            throw e
+                        }
+                    }
+                },
                 onExit = { finish() },
             )
         }
