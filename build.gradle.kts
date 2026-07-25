b789e489d6820d2dfde01370e615b28cbb852171
ai-kurou <269343831+ai-kurou@users.noreply.github.com>
2026-07-25 15:11:52 +0900

    webpack-dev-serverを5.2.5に固定

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ plugins.withType<YarnPlugin> {
     extensions.configure<YarnRootExtension> {
         resolution("serialize-javascript", "7.0.5")
         resolution("uuid", "11.1.1")
+        resolution("webpack-dev-server", "5.2.5")
     }
 }
 
