48229ef62ce5cb3f018289d5c7d3bafcd3ff7912
ai-kurou <269343831+ai-kurou@users.noreply.github.com>
2026-08-03 16:20:55 +0900

    test: cover feedback detail app navigation

--- app/androidApp/src/androidTest/kotlin/kurou/kodriver/MainActivityTest.kt
+++ app/androidApp/src/androidTest/kotlin/kurou/kodriver/MainActivityTest.kt
@@ -146,6 +146,7 @@ class MainActivityTest {
         clickScrollableItem("テーマ")
         clickItem("キャンセル")
         clickScrollableItem("ダイナミックカラー")
+        clickItemAndNavigateBack("フィードバックを送信")
         clickItemAndNavigateBack("ライセンス")
     }
 
