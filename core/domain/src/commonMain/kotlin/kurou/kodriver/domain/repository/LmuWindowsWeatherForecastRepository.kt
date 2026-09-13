5cb14d081905d41a5c618f09f80a3be36d5b6edb
ai-kurou <269343831+ai-kurou@users.noreply.github.com>
2026-09-13 23:31:29 +0900

    chore: 天候予報機能の実装を削除 (#1514)

--- core/domain/src/commonMain/kotlin/kurou/kodriver/domain/repository/LmuWindowsWeatherForecastRepository.kt
+++
@@ -1,7 +0,0 @@
-package kurou.kodriver.domain.repository
-
-import kurou.kodriver.domain.model.LmuWindowsWeatherForecast
-
-interface LmuWindowsWeatherForecastRepository {
-    suspend fun weatherForecasts(): List<LmuWindowsWeatherForecast>
-}
